f4a5d9b8e152d02bddbfa8a1cb96877b00bd4833
Denali Relles <denali.relles@mail.mcgill.ca>
2020-08-23 22:12:59 -0400

    changed some stuff. Halfway through implementing emoji but I'm not sure I like it

--- bar-script.rb
+++ bar-script.rb
@@ -1,7 +1,15 @@
 
 
-def make_button(text, command, button)
-	return "\%{#{button}:" + command + ":}" + text + "\%{A}"
+def make_button(text, commands)
+	
+	prefixes = ''
+	
+	commands.each do |button, action|
+		prefixes = prefixes + "\%{#{button}:#{action}:}"
+	end
+	
+	
+	return "#{prefixes} #{text} #{'%{A}' * commands.size}"
 end
 
 def basic_colors()
@@ -31,7 +39,7 @@ def get_battery()
 		end
 	end
 	
-	return "\%{F\##{fg_color}}\%{B\##{bg_color}}Battery: " + battery_level + '%{B-}'
+	return "\%{F\##{fg_color}}\%{B\##{bg_color}}Battery: #{battery_level}\%{B-}"
 end
 
 
@@ -44,36 +52,36 @@ def get_volume()
 		mute_string = "OO"
 	end
 	
-	down_button = '%{B#700000}' + make_button('VV', 'vol down', 'A') + '%{B-}'
+	down_button = "\%{B\#700000}#{make_button('VV', {'A' => 'vol down'})}\%{B-}"
 	
-	up_button = '%{B#700000}' + make_button('AA', 'vol up', 'A') + '%{B-}'
+	up_button = "\%{B\#700000}#{make_button('AA', {'A' => 'vol up'})}\%{B-}"
 	
-	mute_button = '%{B#700000}' + make_button(mute_string, 'vol flip', 'A') + '%{B-}'
+	mute_button = "\%{B\#700000}#{make_button(mute_string, {'A' => 'vol flip'})}\%{B-}"
 	
 	
-	return "\%{B-}\%{F#bbbbff}Volume: " + down_button + " #{output[0]} " + up_button + " " + mute_button
+	return "\%{B-}\%{F#bbbbff}Volume: #{down_button} #{output[0]} #{up_button} #{mute_button}#{basic_colors()}"
 end
 
 def get_brightness()
 	
 	output = `bright get`.chomp
 	
-	down_button = '%{B#000090}' + make_button('VV', 'bright down', 'A') + '%{B-}'
+	down_button = "\%{B\#000090}#{make_button("\u25bc", {'A' => 'bright down'})}\%{B-}"
 	
-	scroll_button = make_button(make_button(output, 'bright up', 'A5'), 'bright down', 'A4')
+	scroll_button = make_button((output.to_i)/100, {'A5' => 'bright up', 'A4' => 'bright down'})
 	
-	up_button = '%{B#000090}' + make_button('AA', 'bright up', 'A') + '%{B-}'
+	up_button = "%{B#000090}#{make_button("\u25b2", {'A' => 'bright up'})}\%{B-}"
 	
-	return "\%{B-}\%{F\#ffff00}Brightness: " + down_button + ' ' + scroll_button + ' ' + up_button
+	return "\%{B-}\%{F\#ffff00}\u263c#{down_button}#{scroll_button}#{up_button}\u263c"
 end
 
 def get_date()
-	output = `date`
-	return basic_colors() + output.chomp
+	output = `date`.chomp
+	return basic_colors() + output
 end
 
 def get_power()
-	suspend_button = make_button('suspend', 'systemctl suspend', 'A')
+	suspend_button = make_button('suspend', {'A' => 'systemctl suspend'})
 	
 	return basic_colors() + '%{R}' + suspend_button
 end
@@ -84,14 +92,16 @@ def get_wifi()
 end
 
 def get_self_destruct()
-	return '%{R}' + make_button('self-destruct', 'pkill lemonbar', 'A') + basic_colors()
+	return '%{R}' + make_button('self-destruct', {'A' => 'pkill lemonbar'}) + basic_colors()
 end
 
 # puts get_battery()
 
 while true
-	puts '%{l}' + get_date() + '  ' + get_battery() + '  ' + get_brightness() + '  ' + get_volume() + '%{r}' + get_wifi() + '  ' + get_self_destruct() +'  ' + get_power() + basic_colors()
+	puts "\%{l}#{get_date()}  #{get_battery()}  #{get_brightness()}  #{get_volume()}\%{r}#{get_wifi()}  #{get_self_destruct()}  #{get_power()}#{basic_colors()}"
 	
 	sleep(1)
 end
+
+# puts "\u25bc".unicode_normalize
 
