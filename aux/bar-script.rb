700a42ea2e74e75a471cb97749a4654fad3c0167
Denali Relles <denali.relles@mail.mcgill.ca>
2021-03-09 10:37:20 -0500

    change diary to emacs, mouse settings, easy hw scripts, better bar updating

--- aux/bar-script.rb
+++ aux/bar-script.rb
@@ -1,4 +1,55 @@
 
+require 'rb-inotify'
+
+class Widget
+  def initialize(updater)
+    @updater = updater
+    @@wBasket.add(self)
+    @printable = method(@updater).call
+  end
+
+  def self.setBasket(aBasket)
+    @@wBasket = aBasket
+  end
+
+  def updateMe
+    @printable = method(@updater).call
+  end
+  
+  def pokeBasket
+    self.updateMe
+    @@wBasket.poke
+  end
+
+  def getPrintable
+    @printable
+  end
+    
+end
+
+class WidgeBasket
+  def initialize
+    @myWidges = []
+  end
+
+  def add(aWidge)
+    @myWidges << aWidge
+  end
+
+  def poke
+    @myWidges.each do |tWidge|
+      print tWidge.getPrintable + "  "
+    end
+    print "\n"
+  end
+
+  def updateAll
+    @myWidges.each do |tW|
+      tW.updateMe
+    end
+  end
+  
+end
 
 def make_button(text, commands)
 	
@@ -21,7 +72,7 @@ def get_battery()
 	
 	battery_level = output[1]
 	
-	if output[0].match(/Cha/)
+	if output[0].match(/Charg/)
 		fg_color = '000000'
 		bg_color = '00ff00'
 	else
@@ -39,22 +90,26 @@ def get_battery()
 		end
 	end
 	
-	return "\%{F\##{fg_color}}\%{B\##{bg_color}}Battery: #{battery_level}\%{B-}"
+	return "\%{F\##{fg_color}}\%{B\##{bg_color}}Battery: #{battery_level}#{basic_colors}"
 end
 
 
 def get_volume()
+
+  mute_string = ""
 	
 	output = `vol get`.split
 	if output[1] == "off"
-		mute_string = "\u00D8"
+	  mute_string = "\u00D8"
+          mute_button = "\%{B\#700000}#{make_button(mute_string, {'A' => 'amixer set Master on > /home/denalir/.scripts/aux/bar_vol.txt'})}\%{B-}"
 	else
-		mute_string = "\u266b"
+	  mute_string = "\u266b"
+          mute_button = "\%{B\#700000}#{make_button(mute_string, {'A' => 'amixer set Master off> /home/denalir/.scripts/aux/bar_vol.txt'})}\%{B-}"
 	end
 	
-	down_button = "\%{B\#700000}#{make_button("\u25bc", {'A' => 'vol down'})}\%{B-}"
-	up_button = "\%{B\#700000}#{make_button("\u25b2", {'A' => 'vol up'})}\%{B-}"
-	mute_button = "\%{B\#700000}#{make_button(mute_string, {'A' => 'vol flip'})}\%{B-}"
+	down_button = "\%{B\#700000}#{make_button("\u25bc", {'A' => 'amixer set Master 5%- > /home/denalir/.scripts/aux/bar_vol.txt'})}\%{B-}"
+	up_button = "\%{B\#700000}#{make_button("\u25b2", {'A' => 'amixer set Master 5%+ > /home/denalir/.scripts/aux/bar_vol.txt'})}\%{B-}"
+	
 	
 	return "\%{B-}\%{F#bbbbff}Volume: #{down_button} #{output[0]} #{up_button} #{mute_button}#{basic_colors()}"
 end
@@ -69,18 +124,18 @@ def get_brightness()
 	
 	up_button = "%{B#000090}#{make_button("\u25b2", {'A' => 'bright up'})}\%{B-}"
 	
-	return "\%{B-}\%{F\#ffff00}\u263c #{down_button}#{scroll_button}#{up_button} \u263c"
+	return "\%{B-}\%{F\#ffff00}\u263c #{down_button}#{scroll_button}#{up_button} \u263c#{basic_colors}"
 end
 
 def get_date()
 	output = `date +"%R:%S %a %d %b %Y"`.chomp
-	return "#{basic_colors}#{output}"
+	return output
 end
 
 def get_power()
 	power_button = make_button('Suspend', {'A' => 'i3exit suspend'})
 	
-	return basic_colors() + '%{R}' + power_button
+	return '%{R}' + power_button + basic_colors()
 end
 
 def get_wifi()
@@ -97,15 +152,46 @@ end
 def get_diary()
 	
 	output = "diary"
-	button_output = make_button(output, {'A' => "todairy today", 'A3' => "todairy yesterday"})
-	return "\%{R}#{button_output}#{basic_colors()}"
+	button_output = make_button(output, {'A' => "todairy today &", 'A3' => "todairy yesterday &"})
+	return "\%{r}\%{R}#{button_output}#{basic_colors()}"
 end
 
+barBasket = WidgeBasket.new
+Widget.setBasket(barBasket)
+
+clockWidge = Widget.new(:get_date)
+batWidge = Widget.new(:get_battery)
+brightWidge = Widget.new(:get_brightness)
+volWidge = Widget.new(:get_volume)
+diaryWidge = Widget.new(:get_diary)
+wifiWidge = Widget.new(:get_wifi)
+sdWidge = Widget.new(:get_self_destruct)
+powerWidge = Widget.new(:get_power)
+
+barBasket.poke
+
+Thread.new do
+  bNotifier = INotify::Notifier.new
+  bNotifier.watch("/sys/class/backlight/intel_backlight/brightness", :modify) do
+    brightWidge.pokeBasket
+  end
+  bNotifier.run
+end
+
+Thread.new do
+  vNot = INotify::Notifier.new
+  vNot.watch("/home/denalir/.scripts/aux/bar_vol.txt", :modify) do
+    volWidge.pokeBasket
+  end
+  vNot.run
+end
+
+#puts "hi"
 
 while true
-	puts "\%{l}#{get_date()}  #{get_battery()}  #{get_brightness()}  #{get_volume()}\%{r}#{get_diary()} #{get_wifi()}  #{get_self_destruct()}  #{get_power()}#{basic_colors()}"
-	
-	sleep(1)
+  barBasket.updateAll
+  barBasket.poke
+  sleep(1)
 end
 
 
--- aux/vol.rb
+++ aux/vol.rb
@@ -7,24 +7,25 @@ if ARGV.length < 1 || ARGV.length > 1
 	exit(0)
 end
 
-output = `amixer get Master`.split("\n")[5].split
-
-vol_level = output[4].delete_prefix('[').delete_suffix('%]').to_i
-
 if ARGV[0] == "get"
-	puts vol_level.to_s + " " + (output[5].delete_prefix('[').delete_suffix(']'))
+  output = `amixer get Master`.split("\n")[5].split
+
+  vol_level = output[4].delete_prefix('[').delete_suffix('%]').to_i
+
+  puts vol_level.to_s + " " + (output[5].delete_prefix('[').delete_suffix(']'))
 else
 	if ARGV[0] == "up"
-		set_str = "#{vol_level < 100 - increment ? vol_level + increment : 100 }\%"
+		set_str = "5%+"
 	elsif ARGV[0] == "down"
-		set_str = "#{vol_level > increment ? vol_level - increment : 0 }\%"
+		set_str = "5%-"
 	elsif ARGV[0] == "off" || ARGV[0] == "on"
 		set_str = ARGV[0]
 	elsif ARGV[0] == "flip"
-		set_str = output[5] == '[off]'?"on":"off"
+		set_str = output[5] == '[off]' ? "on" : "off"
 	else
 		puts "incorrect usage"
 		exit(0)
 	end
 	`amixer set Master #{set_str}`
 end
+
